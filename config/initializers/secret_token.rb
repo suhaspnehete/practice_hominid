7b4ab10e4f8f34758c105f7ff852214e6594ffc7
Kenneth <kltcalamay@gmail.com>
2010-09-25 19:46:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeHominid::Application.config.secret_token = '16f83529ac31b41d339e33f0b7b6de2474f3ccacd4192c1f1c38492613b790c05a04314388e141ebbdaf07728583086904f4b3308564c21f7a0e517cba9db0c0'
