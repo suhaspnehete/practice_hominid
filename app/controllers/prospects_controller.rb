219bed9f70920595afe73a31b3c8c4c8151a6880
Kenneth <kltcalamay@gmail.com>
2010-09-26 21:29:37 +0800

    Passed ProspectsController specs. Cuke still failing. Need to fix bug on subscribers/index.html

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -6,10 +6,18 @@ class ProspectsController < ApplicationController
   def create
     @prospect = Prospect.new(params[:prospect])
     if @prospect.save
+      subscribe(@prospect)
       flash[:notice] = 'Please verify your email address.'
       redirect_to :action => 'index'
     else
       render :action => 'new'
     end
+  end
+
+  private
+
+  def subscribe(prospect)
+    h = Hominid::Base.new(:api_key => '0f9707027cec5d61e8122b605cd930e1-us1')
+    h.subscribe(h.find_list_id_by_name("NMPH Clients"), prospect.email, {:FNAME => prospect.firstname})
   end
 end
--- spec/controllers/prospects_controller_spec.rb
+++ spec/controllers/prospects_controller_spec.rb
@@ -12,19 +12,19 @@ describe ProspectsController do
 
   describe "POST 'create'" do
     let(:prospect) { mock_model('Prospect').as_null_object }
+    let(:hominid)  { double('Hominid::Base').as_null_object }
+    let(:list_id)  { double('Integer') }
 
     before do
+      @user_info = {'firstname' => 'Kenneth', 'email' => 'k@example.com'}
       Prospect.stub(:new).and_return(prospect)
+      Hominid::Base.stub(:new).and_return(hominid)
+      hominid.stub(:subscribe).and_return(true)
     end
 
     it "creates a new prospect record" do
-      Prospect.should_receive(:new).with('firstname' => 'Kenneth', 'email' => 'k@example.com')
-      post 'create', :prospect => {'firstname' => 'Kenneth', 'email' => 'k@example.com'}
-    end
-    
-    it "saves that record" do
-      prospect.should_receive(:save)
-      post 'create'
+      Prospect.should_receive(:new).with(@user_info)
+      post 'create', :prospect => @user_info
     end
 
     context "when the prospect record saves successfully" do
@@ -32,7 +32,7 @@ describe ProspectsController do
         prospect.stub(:save).and_return(true)
       end
 
-      it "assings flash[:notice] a message" do
+      it "assigns flash[:notice] a message" do
         post 'create'
         flash[:notice].should eq('Please verify your email address.')
       end
@@ -40,6 +40,12 @@ describe ProspectsController do
       it "redirects to the index action" do
         post 'create'
         response.should redirect_to(:action => 'index')
+      end
+
+      it "subscribes the prospect to a mailing list" do
+        hominid.should_receive(:find_list_id_by_name).and_return(list_id)
+        hominid.should_receive(:subscribe).with(list_id, prospect.email, {:FNAME => prospect.firstname})
+        post 'create'
       end
     end
 
