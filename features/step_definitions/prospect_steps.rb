8f987770cd63aac1fca80d9a2cb7ce365c311568
Kenneth <kltcalamay@gmail.com>
2010-09-26 12:34:57 +0800

    Implemented first pending step.

--- features/step_definitions/prospect_steps.rb
+++ features/step_definitions/prospect_steps.rb
@@ -1,3 +1,11 @@
+When /^I opt\-in with "([^"]*)" and "([^"]*)"$/ do |firstname, email|
+  steps %Q{
+    When I fill in "Firstname" with "#{firstname}"
+    And I fill in "Email" with "#{email}"
+    And I press "Submit"
+  }
+end
+
 Then /^I should be redirected$/ do
   follow_redirect!
 end
